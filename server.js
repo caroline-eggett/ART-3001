e6c498e077d445aa2bc4a449a125652ead749bda
Caroline <caroline.eggett@gmail.com>
2022-01-24 23:00:47 -0500

    fix heroku error, hopefully

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
+require('dotenv').config()
+const port = (process.env.PORT || 5000); 
 
 app.get('/', function (req, res) {
     res.sendFile('index.html', {root: __dirname })
 })
 
-app.listen(8081, () => {   
+app.listen(port, () => {   
    console.log("Example app listening");
 })
